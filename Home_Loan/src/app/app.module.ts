4f5a7e374ece426e8c1b168dc2ba44c8e2c5db88
Rutuja-Ahirekar <rutujabhosale717@gmail.com>
2024-03-05 10:09:28 -0800

    new commit

--- Home_Loan/src/app/app.module.ts
+++ Home_Loan/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
+import { DashboardComponent } from './template/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
