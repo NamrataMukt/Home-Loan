2733fb4e3ef2097f61adc7b75b6f433e99a9e537
khachanemonika <monikakhachane03@gmail.com>
2024-03-05 13:43:59 +0530

    by moni

--- Home_Loan/src/app/app-routing.module.ts
+++ Home_Loan/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './template/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {
+    path:'',redirectTo:'homeloan',pathMatch:'full'
+  },
+  {
+  path:'homeloan',component:DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
